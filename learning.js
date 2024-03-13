20199f30ce415bdfb310b592966ee1dad8dbeed1
Aqsa Zaboor <aqsazaboor786@gmail.com>
2024-03-13 14:47:11 +0500

    first commit

---
+++ learning.js
@@ -0,0 +1,15 @@
+var theacherName = "azamzia";
+console.log(theacherName);
+
+// values
+// 42; //number
+// 3.43;
+
+// true;
+// false;
+
+// undefined;
+// null[(1, 2, 3)]; //Array
+// {
+//   name: "harrys";
+// }
